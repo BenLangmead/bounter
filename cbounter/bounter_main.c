fbcde6b1e0871b263ac4c67de191b834947b8126
Ben Langmead <langmea@cs.jhu.edu>
2018-07-03 10:50:31 -0400

    add Xcode project

---
+++ cbounter/bounter_main.c
@@ -0,0 +1,9 @@
+//
+//  bounter_main.c
+//  bounter
+//
+//  Created by Ben Langmead on 7/3/18.
+//  Copyright © 2018 Ben Langmead. All rights reserved.
+//
+
+#include "bounter_main.h"
---
+++ cbounter/bounter_main.h
@@ -0,0 +1,14 @@
+//
+//  bounter_main.h
+//  bounter
+//
+//  Created by Ben Langmead on 7/3/18.
+//  Copyright © 2018 Ben Langmead. All rights reserved.
+//
+
+#ifndef bounter_main_h
+#define bounter_main_h
+
+#include <stdio.h>
+
+#endif /* bounter_main_h */
